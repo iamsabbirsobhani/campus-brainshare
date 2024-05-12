d0e1209e7cbde95f6b090493106728e8970f3178
iamsabbirsobhani <sabbirsobhani@gmail.com>
2024-05-13 01:12:49 +0600

    add Hero section

---
+++ resources/views/components/hero/hero.blade.php
@@ -0,0 +1,27 @@
+<div class=" bg-blue-500/10 text-gray-700 flex justify-center gap-10 p-10 rounded-[40px] max-w-[80vw] m-auto items-center">
+    <!-- If you do not have a consistent goal in life, you can not live it in a consistent way. - Marcus Aurelius -->
+    <!-- texture -->
+    <div class="">
+        <h1 class="text-6xl font-bold mb-5">Focusing on knowledge <br>
+            and skill sharing within <br>
+            EWU Campus</h1>
+        <svg xmlns="http://www.w3.org/2000/svg" width="558.964" height="37.268" viewBox="0 0 558.964 37.268">
+            <path id="Path_1" data-name="Path 1" d="M160,663.135S206.309,635,253.2,651.078s144.684-14.736,144.684-14.736,57.606-22.774,125.929,14.736,194.252-14.736,194.252-14.736" transform="translate(-159.481 -628.485)" fill="none" stroke="#374151" stroke-width="2" />
+        </svg>
+
+        <p class=" mt-5">Unlocking knowledge, empowering peers. Share your spark, ignite - <br>
+            your campus. Brains together, success achieved.</p>
+
+        <div class="mt-5">
+            <button class="bg-blue-500 text-white text-xl font-bold uppercase p-4 w-96 rounded-full">Create Free Account</button>
+        </div>
+    </div>
+    <!-- image -->
+    <div class="grid gap-4 grid-cols-[260px_200px_200px] grid-rows-[250px_200px_200px]">
+        <img src=" {{URL::asset('/images/hero-section/2.png')}}" alt="hero-image" class="h-full rounded-2xl" />
+        <img src=" {{URL::asset('/images/hero-section/1.png')}}" alt="hero-image" class=" rounded-2xl" />
+        <img src=" {{URL::asset('/images/hero-section/3.png')}}" alt="hero-image" class=" rounded-2xl" />
+        <img src=" {{URL::asset('/images/hero-section/4.png')}}" alt="hero-image" class=" w-[95%] justify-self-center rounded-2xl" />
+        <img src=" {{URL::asset('/images/hero-section/5.png')}}" alt="hero-image" class="  rounded-2xl col-span-2 " />
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Campus Brainshare</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        @vite('resources/css/app.css')
+    <title>Campus Brainshare</title>
 
-    </head>
-    <body class="font-sans antialiased">
-        <x-navbar.navbar />.
-    </body>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    @vite('resources/css/app.css')
+
+</head>
+
+<body class="font-sans antialiased">
+    <x-navbar.navbar />.
+    <x-hero.hero />.
+</body>
+
 </html>
